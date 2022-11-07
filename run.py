705dbf810834f0549c240d106b0b37e6a0feeaf6
deenafe <tymbalink@hotmail.com>
2022-11-07 15:06:18 +0000

    Create display_vailable function and welcome customer to store

--- run.py
+++ run.py
@@ -12,8 +12,23 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('price_list')
 
-prices = SHEET.worksheet("prices")
+def display_available():
+    """
+    Welcomes the customer to the online store.
+    Request customer to input name, then displays items available in the store.
+    Gets the available items from a worksheet.
+    """
+    print("****************************")
+    print("Welcome to Deen's Online Store")
+    print("****************************\n\n")
 
-get_prices = prices.get_all_values()
+    customer_name = input("Please enter your name: ")
+    print(f"\n\nHi {customer_name}. We offer the best quality of consumables and groceries. Please find below, the item presently available in our store.\n")
 
-print(get_prices)
+    print("****************************")
+    print("      Available Items")
+    print("****************************\n")
+
+
+
+display_available()
